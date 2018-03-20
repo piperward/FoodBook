459a25eec37ea5812ee2caf95f35c9d29014318d
Piper Ward <piperward@2ndclass-10-144-75-61.public.utexas.edu>
2018-03-19 19:16:47 -0500

    Created SignInViewController.swift and modified the post screen

---
+++ FoodBook/SignInViewController.swift
@@ -0,0 +1,24 @@
+//
+//  SignInViewController.swift
+//  FoodBook
+//
+//  Created by Piper Ward on 3/19/18.
+//  Copyright © 2018 Team3. All rights reserved.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
