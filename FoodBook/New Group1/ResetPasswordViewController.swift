c7af3356f2b4a924c95729b43f29a7f237e98cbd
Kyle McCarver <kmccarver8@gmail.com>
2018-04-03 17:41:25 -0500

    Reset password implemented

--- FoodBook/New Group1/ResetPasswordViewController.swift
+++ FoodBook/New Group1/ResetPasswordViewController.swift
@@ -7,18 +7,18 @@
 //
 
 import UIKit
+import Firebase
 
 class ResetPasswordViewController: UIViewController {
     
     //MARK: Properties
     @IBOutlet weak var emailTextField: UITextField!
-    
+    @IBOutlet weak var statusLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
-        
+        statusLabel.text! = ""
     }
     
     override func didReceiveMemoryWarning() {
@@ -28,6 +28,18 @@ class ResetPasswordViewController: UIViewController {
     
     //MARK: Actions
     @IBAction func reset(_ sender: Any) {
+        if emailTextField.text! != "" {
+            Auth.auth().sendPasswordReset(withEmail: emailTextField.text!, completion: { error in
+                if error == nil {
+                    self.emailTextField.text! = ""
+                    self.statusLabel.text! = "Password reset email sent."
+                } else {
+                self.statusLabel.text! = "ERROR"
+                }
+            })
+        } else {
+            self.statusLabel.text! = "Please enter a valid email."
+        }
     }
     
     @IBAction func signIn(_ sender: Any) {
