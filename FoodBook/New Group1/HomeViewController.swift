7d1e0dbd192d584af44e9120fd234a290d1e5d70
Piper Ward <piperward@pipers-macbook-pro.local>
2018-04-12 18:11:02 -0500

    Yup

--- FoodBook/New Group1/HomeViewController.swift
+++ FoodBook/New Group1/HomeViewController.swift
@@ -9,84 +9,83 @@
 import UIKit
 import Firebase
 
-public var postList = [Post]()
-
-class HomeViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class HomeViewController: UIViewController {
 
     var postCellIdentifier = "postCellIdentifier"
-    let ref = Database.database().reference(withPath: "posts")
+    var posts = [Post]()
+    var users = [User]()
     
     @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-        tableView.delegate = self
+        //tableView.estimatedRowHeight = 521
+        tableView.rowHeight = UITableViewAutomaticDimension
         tableView.dataSource = self
-        
-        // Attach a listener to receive updates whenever the posts endpoint is modified
-        ref.observe(.value, with: { snapshot in
-            // Store the latest version of the data in a local variable inside the listener’s closure.
-            var newPosts: [Post] = []
-            // Loop through the posts provided by the snapshot that the closure returned, creating list
-            for item in snapshot.children {
-                let post = Post(snapshot: item as! DataSnapshot)
-                newPosts.append(post)
+        loadPosts()
+    }
+    
+    func loadPosts() {
+        if let currentUser = Auth.auth().currentUser {
+            Api.observeFeed(withId: currentUser.uid) { (post) in
+                guard let postUid = post.uid else {
+                    return
+                }
+                self.fetchUser(uid: postUid, completed: {
+                    self.posts.append(post)
+                    self.tableView.reloadData()
+                })
             }
-            
-            // Reassign items to the latest version of the data, reload the table view
-            postList = newPosts
-            self.tableView.reloadData()
-        })
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        tableView.reloadData()
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-    // TableView Methods
-    
-    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        return 1
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return postList.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        let cell = tableView.dequeueReusableCell(withIdentifier: postCellIdentifier, for: indexPath as IndexPath) as! PostTableViewCell
-        let currentPost = postList[indexPath.row]
-        
-        //Use photo URL to set image view
-        let url = URL(string: currentPost.photoUrl)
-        if let data = try? Data(contentsOf: url!) {
-            cell.postImageView.image = UIImage(data: data)!
+            Api.observeFeedRemoved(withId: currentUser.uid) { (post) in
+                self.posts = self.posts.filter { $0.id != post.id }
+                self.users = self.users.filter { $0.id != post.uid }
+                
+                self.tableView.reloadData()
+            }
         }
+    }
+    
+    func fetchUser(uid: String, completed:  @escaping () -> Void ) {
+        Api.observeUser(withId: uid, completion: {
+            user in
+            self.users.append(user)
+            completed()
+        })
         
-        //Set caption and username
-        cell.postDescriptionLabel.text = currentPost.caption
-        cell.postNameLabel.text = "Jane Doe"
-        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "Home_ProfileSegue" {
+            let profileVC = segue.destination as! ProfileUserViewController
+            let userId = sender  as! String
+            profileVC.userId = userId
+        }
+    }
+}
+
+extension HomeViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return posts.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell", for: indexPath) as! PostTableViewCell
+        let post = posts[indexPath.row]
+        let user = users[indexPath.row]
+        cell.post = post
+        cell.user = user
+        cell.delegate = self
         return cell
     }
-    
+}
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+//TODO create these segues
+extension HomeViewController: HomeTableViewCellDelegate {
+    func goToCommentVC(postId: String) {
+        //performSegue(withIdentifier: "CommentSegue", sender: postId)
     }
-    */
-
+    func goToProfileUserVC(userId: String) {
+        //performSegue(withIdentifier: "Home_ProfileSegue", sender: userId)
+    }
 }
